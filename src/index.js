624e15dc90e6c716b9db25ac3a973aacc6960217
the-wrong-guy <kaltbx@gmail.com>
2020-07-27 11:08:06 +0530

    icons and facicons change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import BackToTop from './menu'
-// import App from './App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
