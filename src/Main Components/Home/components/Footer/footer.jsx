324e526072ae4cfdfad82e176ad8724df79e11e7
the-wrong-guy <kaltbx@gmail.com>
2020-08-01 12:21:27 +0530

    links to profile added

--- src/Main Components/Home/components/Footer/footer.jsx
+++ src/Main Components/Home/components/Footer/footer.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import {IconButton} from "@material-ui/core"
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub'
-import FavoriteTwoToneIcon from '@material-ui/icons/FavoriteTwoTone';
 import styles from './footer.module.css'
 function Footer(){
     return(
         <>
             <div>
-                <span className={styles.tagline}>Made with &nbsp; <FavoriteTwoToneIcon className={styles.heart}/></span>
+                <span className={styles.tagline}>Made with  &#10094; &#10095;</span>
                 <div className={styles.icons}>
-                    <InstagramIcon/>&nbsp;&nbsp;
-                    <GitHubIcon/>
+                <IconButton aria-label="" href="https://github.com/the-wrong-guy" target="blank">
+                  <GitHubIcon/>
+                </IconButton>
+                <IconButton aria-label="" href="https://www.instagram.com/________the_wrong_guy_________/" target="blank">
+                <InstagramIcon/>
+                </IconButton>  
                 </div>
             </div>
           
