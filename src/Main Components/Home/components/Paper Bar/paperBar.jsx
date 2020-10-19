495d569aee85b46318debf47b7352a02dee26b6b
the-wrong-guy <kaltbx@gmail.com>
2020-10-19 14:17:31 +0530

    Styling of PaperBar updated

--- src/Main Components/Home/components/Paper Bar/paperBar.jsx
+++ src/Main Components/Home/components/Paper Bar/paperBar.jsx
@@ -2,7 +2,9 @@ import React,{useState,useEffect} from 'react'
 import { Paper, Typography, Grid } from '@material-ui/core'
 import CountUp from 'react-countup';
 import styles from './paperBar.module.css'
-
+import Red from '@material-ui/core/colors/red';
+import Green from '@material-ui/core/colors/green';
+import Orange from '@material-ui/core/colors/deepOrange';
 
 const PaperBar = ({title,data}) => {
     const [TestPerMillon,setTestPerMillon] = useState('')
@@ -25,20 +27,20 @@ const PaperBar = ({title,data}) => {
         <div className={styles.container}>
             <div className={styles.paperContainer}>
                 <Paper elevation={15} className={styles.paper}>
-                    <div><Typography className={styles.typo1} variant="subtitle1" color="initial">Tests per million</Typography></div>
-                    <div><Typography variant="subtitle1" color="initial"><CountUp  start ={0} end={TestPerMillon} duration = {3.5} separator = ","/></Typography></div>
+                    <div><Typography  variant="subtitle1" style={{color:`${Green[500]}`,fontWeight:"700"}}>Tests per million</Typography></div>
+                    <div><Typography variant="subtitle1" style={{color:`${Green[500]}`}}><CountUp  start ={0} end={TestPerMillon} duration = {3.5} separator = ","/></Typography></div>
                 </Paper>
             </div>
             <div  className={styles.paperContainer}>
                 <Paper elevation={15} className={styles.paper}>
-                    <div><Typography className={styles.typo2} variant="subtitle1" color="initial">Total samples tested</Typography></div>
-                    <div><Typography variant="subtitle1" color="initial"><CountUp  start ={0} end={TotalSamplesTested} duration = {3.5} separator = ","/></Typography></div>
+                    <div><Typography  variant="subtitle1" style={{color:`${Orange[500]}`,fontWeight:"700"}}>Total samples tested</Typography></div>
+                    <div><Typography variant="subtitle1" style={{color:`${Orange[500]}`}}><CountUp  start ={0} end={TotalSamplesTested} duration = {3.5} separator = ","/></Typography></div>
                 </Paper>
             </div>
             <div className={styles.paperContainer}>
                 <Paper elevation={15} className={styles.paper}>
-                    <div><Typography className={styles.typo3} variant="subtitle1" color="initial">Positivity rate</Typography></div>
-                    <div><Typography variant="subtitle1" color="initial">{PositivityRate}</Typography></div>
+                    <div><Typography  variant="subtitle1" style={{color:`${Red[600]}`,fontWeight:"700"}}>Positivity rate</Typography></div>
+                    <div><Typography variant="subtitle1" style={{color:`${Red[500]}`}}>{PositivityRate}</Typography></div>
                 </Paper>
             </div>
         </div>
