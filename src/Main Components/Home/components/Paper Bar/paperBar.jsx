1ccc9ada7b7c0fcd8176e3d572590dc4473a81a4
the-wrong-guy <kaltbx@gmail.com>
2021-03-23 15:04:12 +0530

    bugs fixed

--- src/Main Components/Home/components/Paper Bar/paperBar.jsx
+++ src/Main Components/Home/components/Paper Bar/paperBar.jsx
@@ -1,59 +1,96 @@
-import React,{useState,useEffect} from 'react'
-import { Paper, Typography, Grid } from '@material-ui/core'
-import CountUp from 'react-countup';
-import styles from './paperBar.module.css'
-import Red from '@material-ui/core/colors/red';
-import Green from '@material-ui/core/colors/green';
-import Orange from '@material-ui/core/colors/deepOrange';
+import React, { useState, useEffect } from "react";
+import { Paper, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import styles from "./paperBar.module.css";
+import Red from "@material-ui/core/colors/red";
+import Green from "@material-ui/core/colors/green";
+import Orange from "@material-ui/core/colors/deepOrange";
 import numeral from "numeral";
 
+const PaperBar = ({ title, data }) => {
+  const [TestPerMillon, setTestPerMillon] = useState("");
+  const [TotalSamplesTested, setTotalSamplesTested] = useState("");
+  const [PositivityRate, setPositivityRate] = useState("");
 
-const PaperBar = ({title,data}) => {
-    const [TestPerMillon,setTestPerMillon] = useState('')
-    const [TotalSamplesTested,setTotalSamplesTested] = useState('')
-    const [PositivityRate,setPositivityRate] = useState('')
-
-    useEffect(()=>{
-        fetch('https://api.covid19india.org/data.json')
-        .then(response=>response.json())
-        .then(data=>{
-            let localdta = data.tested[data.tested.length-1]
-            // console.log(localdta)
-            setPositivityRate(localdta.testpositivityrate)
-            setTestPerMillon(localdta.testspermillion)
-            setTotalSamplesTested(localdta.totalsamplestested)
-        }) 
-        .catch(err=>console.log(err))
-    },[])
-    return (
-        <div className={styles.container}>
-            <div className={styles.paperContainer}>
-                {TestPerMillon==='' || undefined ? '': (
-                <Paper elevation={15} className={styles.paper}>
-                    <div><Typography  variant="subtitle1" style={{color:`${Green[500]}`,fontWeight:"700"}}>Tests per million</Typography></div>
-                    <div><Typography variant="subtitle1" style={{color:`${Green[500]}`}}>{numeral(TestPerMillon).format("0a")}</Typography></div>
-                </Paper>
-                ) }
+  useEffect(() => {
+    fetch("https://api.covid19india.org/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let localdta = data.tested[data.tested.length - 1];
+        // console.log(localdta)
+        setPositivityRate(localdta.testpositivityrate);
+        setTestPerMillon(localdta.testspermillion);
+        setTotalSamplesTested(localdta.totalsamplestested);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className={styles.container}>
+      <div className={styles.paperContainer}>
+        {TestPerMillon && (
+          <Paper elevation={15} className={styles.paper}>
+            <div>
+              <Typography
+                variant='subtitle1'
+                style={{ color: `${Green[500]}`, fontWeight: "700" }}
+              >
+                Tests per million
+              </Typography>
             </div>
-            <div  className={styles.paperContainer}>
-                {TotalSamplesTested === '' || undefined ? '' : (   
-                <Paper elevation={15} className={styles.paper}>
-                    <div><Typography  variant="subtitle1" style={{color:`${Orange[500]}`,fontWeight:"700"}}>Total samples tested</Typography></div>
-                    <div><Typography variant="subtitle1" style={{color:`${Orange[500]}`}}>{numeral(TotalSamplesTested).format("0a")}</Typography></div>
-                </Paper>
-                )} 
+            <div>
+              <Typography
+                variant='subtitle1'
+                style={{ color: `${Green[500]}` }}
+              >
+                {numeral(TestPerMillon).format("0a")}
+              </Typography>
             </div>
-            <div className={styles.paperContainer}>
-                {PositivityRate === '' || undefined ? '' : (   
-                <Paper elevation={15} className={styles.paper}>
-                    <div><Typography  variant="subtitle1" style={{color:`${Red[600]}`,fontWeight:"700"}}>Positivity rate</Typography></div>
-                    <div><Typography variant="subtitle1" style={{color:`${Red[600]}`}}>{PositivityRate}</Typography></div>
-                </Paper>
-                )} 
+          </Paper>
+        )}
+      </div>
+      <div className={styles.paperContainer}>
+        {TotalSamplesTested && (
+          <Paper elevation={15} className={styles.paper}>
+            <div>
+              <Typography
+                variant='subtitle1'
+                style={{ color: `${Orange[500]}`, fontWeight: "700" }}
+              >
+                Total samples tested
+              </Typography>
             </div>
-        </div>
-    )
-}
+            <div>
+              <Typography
+                variant='subtitle1'
+                style={{ color: `${Orange[500]}` }}
+              >
+                {numeral(TotalSamplesTested).format("0a")}
+              </Typography>
+            </div>
+          </Paper>
+        )}
+      </div>
+      <div className={styles.paperContainer}>
+        {PositivityRate && (
+          <Paper elevation={15} className={styles.paper}>
+            <div>
+              <Typography
+                variant='subtitle1'
+                style={{ color: `${Red[600]}`, fontWeight: "700" }}
+              >
+                Positivity rate
+              </Typography>
+            </div>
+            <div>
+              <Typography variant='subtitle1' style={{ color: `${Red[600]}` }}>
+                {PositivityRate}
+              </Typography>
+            </div>
+          </Paper>
+        )}
+      </div>
+    </div>
+  );
+};
 
-
-export default PaperBar
+export default PaperBar;
--- src/Main Components/Prevention/prevention.jsx
+++ src/Main Components/Prevention/prevention.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
-import styles from './prevention.module.css'
-
+import styles from "./prevention.module.css";
+var Spinner = require("react-spinkit");
 export default function Prevention() {
   return (
     <div className={styles.container}>
-    <Typography className={styles.header} variant="h5" color="initial">Prevention</Typography>
+      <Typography className={styles.header} variant='h5' color='initial'>
+        Prevention
+      </Typography>
       <iframe
         className={styles.iframe}
-        src="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html"
-        title="COVID_Prevention_Measures"
+        src='https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html'
+        title='COVID Prevention Measures'
       />
     </div>
   );
